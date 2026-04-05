a9753e5fa3fb8d036a24e549feedd94cfb222c50
KrauXO <saburorafqi@gmail.com>
2026-04-05 13:18:04 +0700

    Tugas Pertemuan 1 & 2 Selesai

---
+++ Pertemuan1/GL_LINE_STRIP/main.cpp
@@ -0,0 +1,27 @@
+#include <GL/glut.h>
+
+void strip(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLineWidth(4.0f);
+	glBegin(GL_LINE_STRIP);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(0.25f, 0.25f, 0.0f);
+		glVertex3f(-0.25f, -0.25f, 0.0f);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(-0.25f, 0.25f, 0.0f);
+		glVertex3f(0.25f, -0.25f, 0.0f);
+	glEnd();
+	glFlush();
+}
+
+int main (int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	glutInitWindowSize(640, 480);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutCreateWindow("GL_LINES_STRIP");
+	glutDisplayFunc(strip);
+	glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
+	glutMainLoop();
+}
